faf346aa298bf887d0623b8fb25468dd8614f7d6
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-07-11 23:27:02 +0700

    add Agora Service, agoraConfig, add JoinRoomResponse

--- src/dto/request/JoinRoomRequestDTO.ts
+++ src/dto/request/JoinRoomRequestDTO.ts
@@ -1,5 +1,9 @@
+import { JoinChannelAgoraRequest } from "models/AgoraModel";
+import { RoomModel } from "models/RoomModel";
+import { UserModel } from "models/UserModel";
 
 export interface JoinRoomRequestDTO {
     user: UserModel;
-    room: Pick
+    room: Pick<RoomModel, "roomCode">;
+    agora: JoinChannelAgoraRequest;
 }
